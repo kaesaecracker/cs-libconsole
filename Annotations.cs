5d3b47722f05a885380c924c2a698686d29af840
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2018-05-14 16:36:48 +0200

    console runner annotations

---
+++ Annotations.cs
@@ -0,0 +1,38 @@
+﻿namespace Library
+{
+    using System;
+
+    public abstract class NumberNameDescriptionAttribute : Attribute
+    {
+        public NumberNameDescriptionAttribute(string number,
+                                              string name,
+                                              string description)
+        {
+            this.Number = number;
+            this.Name = name;
+            this.Description = description;
+        }
+
+        public string Name { get; }
+        public string Number { get; }
+        public string Description { get; }
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ExerciseAttribute : NumberNameDescriptionAttribute
+    {
+        public ExerciseAttribute(string number, string name, string description = "")
+            : base(number, name, description)
+        {
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class TaskAttribute : NumberNameDescriptionAttribute
+    {
+        public TaskAttribute(string number, string name, string description = "")
+            : base(number, name, description)
+        {
+        }
+    }
+}
